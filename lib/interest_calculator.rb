f9b7bf382805631e9f885a27286f4f8807edc7f5
Katerina <katerinaldg@gmail.com>
2017-04-18 10:09:46 +0100

    Refactored code to be more generic

--- lib/interest_calculator.rb
+++ lib/interest_calculator.rb
@@ -1,13 +1,7 @@
 class InterestCalculator
 
 	def get_interest(rate, principal_amount)
-		if principal_amount == 0
-			0
-		elsif principal_amount == 1
-			0.01
-		else
-			0.02
-		end
+		divided_rate = rate.to_f / 100
+		divided_rate * principal_amount
 	end
-
 end
