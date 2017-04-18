27ea3e5103516678b4527d6f55196bb4a62c1ac8
Katerina <katerinaldg@gmail.com>
2017-04-18 10:07:49 +0100

    corrected some tests

--- spec/interest_calculator_spec.rb
+++ spec/interest_calculator_spec.rb
@@ -7,9 +7,14 @@ describe InterestCalculator do
 		expect(interest_calculator.get_interest(0, 0)).to eq(0)
 	end
 
-	it "returns 1 if rate * principle is 1" do
+	it "returns 0.01 if rate * principle is 1" do
 		interest_calculator = InterestCalculator.new
-		expect(interest_calculator.get_interest(1, 1)).to eq(1)
+		expect(interest_calculator.get_interest(1, 1)).to eq(0.01)
+	end
+
+	it "returns 0.02 if rate * principle is 2" do
+		interest_calculator = InterestCalculator.new
+		expect(interest_calculator.get_interest(1, 2)).to eq(0.02)
 	end
 
 end
